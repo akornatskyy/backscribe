5e458d4f3e8572f2ebffcb737cf926c25d287ff0
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2025-09-13 20:01:13 +0300

    chore: use sh noop for edge case with no archives.

--- builders/group.go
+++ builders/group.go
@@ -10,7 +10,13 @@ import (
 func BuildGroup(group domain.Group) string {
 	src := []string{fmt.Sprintf("\nbackup_%s() {", group.Name)}
 	for _, archive := range group.Archives {
-		src = append(src, BuildArchive(archive, group))
+		chunk := BuildArchive(archive, group)
+		if chunk != "" {
+			src = append(src, chunk)
+		}
+	}
+	if len(src) == 1 {
+		src = append(src, "\n  :\n")
 	}
 	src = append(src, "}")
 	return strings.Join(src, "")
