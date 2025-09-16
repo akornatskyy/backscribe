171c84a6c49bc26670d2e45b00ae7f3811dd4edb
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2025-09-16 20:26:55 +0300

    chore: sanitize function name; use strings builder.

--- builders/group.go
+++ builders/group.go
@@ -8,16 +8,39 @@ import (
 )
 
 func BuildGroup(group domain.Group) string {
-	src := []string{fmt.Sprintf("\nbackup_%s() {", group.Name)}
+	var b strings.Builder
+	b.Grow(1024 * len(group.Archives))
+
+	fmt.Fprintf(&b, "\nbackup_%s() {", sanitizeName(group.Name))
+	added := false
 	for _, archive := range group.Archives {
-		chunk := BuildArchive(archive, group)
-		if chunk != "" {
-			src = append(src, chunk)
+		if chunk := BuildArchive(archive, group); chunk != "" {
+			b.WriteString(chunk)
+			added = true
 		}
 	}
-	if len(src) == 1 {
-		src = append(src, "\n  :\n")
+
+	if !added {
+		b.WriteString("\n  :\n")
 	}
-	src = append(src, "}")
-	return strings.Join(src, "")
+	b.WriteRune('}')
+	return b.String()
+}
+
+func sanitizeName(name string) string {
+	var sb strings.Builder
+	sb.Grow(len(name))
+
+	for _, r := range name {
+		switch {
+		case r == '_',
+			r >= '0' && r <= '9',
+			r >= 'A' && r <= 'Z',
+			r >= 'a' && r <= 'z':
+			sb.WriteRune(r)
+		default:
+			sb.WriteRune('_')
+		}
+	}
+	return sb.String()
 }
